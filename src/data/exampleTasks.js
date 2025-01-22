7e149e95ead77cfdaa2251538dced204e51f72dd
Marcin Osojca <osojca.marcin1@gmail.com>
2025-01-22 18:01:49 +0100

    exampleTasks.js in new "data" folder created

---
+++ src/data/exampleTasks.js
@@ -0,0 +1,8 @@
+
+const tasksTable = [
+    { id: 1, content: "przejść na Reacta", status: "toDo" },
+    { id: 2, content: "zjeść obiad", status: "done" },
+    { id: 3, content: "zrobić kolacje", status: "done" },
+  ];
+
+  export default tasksTable;
