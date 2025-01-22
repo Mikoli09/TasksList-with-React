a0d92c0da367177ea1c631b0502687cf2dc10681
Marcin Osojca <osojca.marcin1@gmail.com>
2025-01-22 18:09:27 +0100

    Tasks index.js refactored

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,6 +1,5 @@
 import "./style.css"
 
-
 const checkIfHide = (elementStatus, variableStatus) => {
     if (!variableStatus) {
         return "";
@@ -12,20 +11,18 @@ const checkmarkVisible = elementStatus => elementStatus === "done" ? "✔" : "";
 
 const checkIfLineThrough = elementStatus => elementStatus === "done" ? "content__done" : "";
 
-
-const Tasks = (props) => (
+const Tasks = ({ tasks, hideDoneTasks }) => (
     <div className="section__container">
-    <ul className="tasks">
-        {props.tasks.map(task => (
-            <li key={task.id} className={`task${checkIfHide(task.status, props.hideDoneTasks)}`}>
-                <button className="listButton">{checkmarkVisible(task.status)}</button>
-                <span className={`content ${checkIfLineThrough(task.status)}`}>{task.content}</span>
-                <button className="listButton listButton--remove">🗑</button>
-            </li>
-        ))}
-    </ul>
+        <ul className="tasks">
+            {tasks.map(task => (
+                <li key={task.id} className={`task${checkIfHide(task.status, hideDoneTasks)}`}>
+                    <button className="listButton">{checkmarkVisible(task.status)}</button>
+                    <span className={`content ${checkIfLineThrough(task.status)}`}>{task.content}</span>
+                    <button className="listButton listButton--remove">🗑</button>
+                </li>
+            ))}
+        </ul>
     </div>
 );
-
 
 export default Tasks;
