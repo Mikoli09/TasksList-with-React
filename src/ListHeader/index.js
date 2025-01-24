d31e943c36be096275d359e2361f044af94f3290
Marcin Osojca <osojca.marcin1@gmail.com>
2025-01-24 15:04:38 +0100

    functions moved inside ListHeaderComponent

--- src/ListHeader/index.js
+++ src/ListHeader/index.js
@@ -1,22 +1,20 @@
 import "./style.css";
 
-const headerButtonVisibility = (tasksTable) => tasksTable.length !== 0 ? "" : "task__hidden";
 
-const textSwap = (hideDoneTasks) => hideDoneTasks ? "Pokaż ukryte" : "Ukryj ukończone";
+const ListHeader = ({ tasks, hideDoneTasks }) => {
+    const headerButtonVisibility = (tasksTable) => tasksTable.length !== 0 ? "" : "task__hidden";
+    const textSwap = (hideDoneTasks) => hideDoneTasks ? "Pokaż ukryte" : "Ukryj ukończone";
+    const everyTaskFinished = (tasksTable) => {
+        const checkIfAllDone = ({ status }) => status === "done";
+        return tasksTable.every(checkIfAllDone) ? true : false;
+    };
 
-const everyTaskFinished = (tasksTable) => {
 
-    const checkIfAllDone = ({ status }) => status === "done";
-
-    return tasksTable.every(checkIfAllDone) ? true : false;
-};
-
-const ListHeader = ({tasks, hideDoneTasks}) => (
-    <div className="header__container">
+    return (<div className="header__container">
         <span className="header__span">Lista zadań</span>
         <button className={`header__button ${headerButtonVisibility(tasks)}`}>{textSwap(hideDoneTasks)}</button>
         <button disabled={everyTaskFinished(tasks)} className={`header__button ${headerButtonVisibility(tasks)}`}>Ukończ wszystkie</button>
     </div>
-);
-
+    );
+}
 export default ListHeader
