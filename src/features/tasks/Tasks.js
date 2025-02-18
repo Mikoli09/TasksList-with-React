3517032ec5139974974350a40da91c0d9fc56541
Marcin Osojca <osojca.marcin1@gmail.com>
2025-02-18 20:22:40 +0100

    Tasks.js updated

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -5,23 +5,20 @@ import Section from "../../common/Section";
 import MainHeader from "../../common/MainHeader";
 import Container from "../../common/Container";
 
-function Tasks() {
-
-  return (
-    <Container>
-      <MainHeader
-        title={"Lista zadań"}
-      />
-      <Section
-        headerContent={"Dodaj nowe zadanie"}
-        sectionContent={<Form />}
-      />
-      <Section
-        headerContent={<ListHeader />}
-        sectionContent={<TaskList />}
-      />
-    </Container>
-  );
-}
+const Tasks = () => (
+  <Container>
+    <MainHeader
+      title={"Lista zadań"}
+    />
+    <Section
+      headerContent={"Dodaj nowe zadanie"}
+      sectionContent={<Form />}
+    />
+    <Section
+      headerContent={<ListHeader />}
+      sectionContent={<TaskList />}
+    />
+  </Container>
+);
 
 export default Tasks;
